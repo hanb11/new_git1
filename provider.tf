e95ad3fbaf6918ff2d89e1ac83e840cda6133dfb
hanb11 <hanumanth.hvb17@gmail.com>
2025-07-27 21:03:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    azure = {
+      source = "hashicorp/azurerm"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+
+  }
+}
 
